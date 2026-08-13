985b36a9f12bbb1f760da5754925588c0537c925
Pepo <pepojimenezbethencourt@gmail.com>
2026-08-13 16:06:29 +0000

    fix: read coder state atomically in burnout monitor

--- src/monitor_utils.c
+++ src/monitor_utils.c
@@ -6,36 +6,63 @@
 /*   By: josjimen <josjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 10:02:53 by josjimen          #+#    #+#             */
-/*   Updated: 2026/08/05 10:48:31 by josjimen         ###   ########.fr       */
+/*   Updated: 2026/08/13 15:58:08 by josjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+static void	get_coder_state_snapshot(
+	t_coder *coder,
+	int *completed_compiles,
+	long *last_compile_time
+)
+{
+	pthread_mutex_lock(&coder->state_mutex);
+	*completed_compiles = coder->completed_compiles;
+	*last_compile_time = coder->last_compile_time;
+	pthread_mutex_unlock(&coder->state_mutex);
+}
+
+static bool	check_coder_burnout(
+	t_simulation *simulation, t_coder *coder, long current_time
+)
+{
+	int		completed_compiles;
+	long	last_compile_time;
+	long	deadline;
+
+	get_coder_state_snapshot(
+		coder,
+		&completed_compiles,
+		&last_compile_time
+		);
+	if (
+		completed_compiles
+		>= simulation->config.number_of_compiles_required
+	)
+		return (false);
+	deadline = last_compile_time + simulation->config.time_to_burnout;
+	if (current_time >= deadline)
+	{
+		set_simulation_finished(simulation);
+		log_state(coder, LOG_BURNOUT);
+		return (true);
+	}
+	return (false);
+}
+
 bool	has_any_coder_burned_out(t_simulation *simulation, long current_time)
 {
 	int		i;
 	t_coder	*coder;
-	long	last_compile_time;
-	long	deadline;
 
 	i = 0;
 	while (i < simulation->config.number_of_coders)
 	{
 		coder = &simulation->coders[i];
-		if (has_completed_required_compiles(coder))
-		{
-			i++;
-			continue ;
-		}
-		last_compile_time = get_last_compile_time(coder);
-		deadline = last_compile_time + simulation->config.time_to_burnout;
-		if (current_time >= deadline)
-		{
-			set_simulation_finished(simulation);
-			log_state(coder, LOG_BURNOUT);
+		if (check_coder_burnout(simulation, coder, current_time))
 			return (true);
-		}
 		i++;
 	}
 	return (false);
